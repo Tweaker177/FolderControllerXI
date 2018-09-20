ff5c69d6d745865bb95b260a34205a858d2d27b1
i0s_tweak3r <djvs23@gmail.com>
2018-09-20 17:49:01 -0400

    Add files via upload

---
+++ FolderController/foldercontroller.mm
@@ -0,0 +1,87 @@
+#import <Preferences/Preferences.h>
+#import <spawn.h>
+#import <objc/runtime.h>
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
+#import <Preferences/PSTableCell.h>
+
+@interface FolderControllerListController: PSListController 
+
+-(void)respring:(id)sender;
+/**
+-(void)respring;
+**/
+-(void)twitter;
+-(void)Paypal;
+
+
+@end
+/*
+@interface PSControlTableCell : PSTableCell
+- (UIControl *)control;
+@end
+*/
+@interface PSSwitchTableCell : PSControlTableCell
+- (id)initWithStyle:(int)style reuseIdentifier:(id)identifier specifier:(id)specifier;
+@end
+
+@interface SRSwitchTableCell : PSSwitchTableCell
+@end
+
+@implementation SRSwitchTableCell
+
+-(id)initWithStyle:(int)style reuseIdentifier:(id)identifier specifier:(id)specifier { 
+//init method
+
+	self = [super initWithStyle:style reuseIdentifier:identifier specifier:specifier]; 
+//call the super init method
+	
+if (self) {
+		[((UISwitch *)[self control]) setOnTintColor:[UIColor blueColor]]; 
+
+//change the switch color
+
+	}
+	return self;
+}
+
+@end
+
+
+@implementation FolderControllerListController
+- (id)specifiers {
+	if(_specifiers == nil) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"FolderController" target:self] retain];
+	}
+	return _specifiers;
+}
+
+- (void)respring:(id)sender {
+	pid_t pid;
+    const char* args[] = {"killall", "SpringBoard", NULL};
+    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)args, NULL);
+}
+
+
+- (void)twitter {
+    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitter://user?screen_name=brianvs"]]) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"twitter://user?screen_name=brianvs"]];
+    } else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetbot:///user_profile/brianvs"]]) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"tweetbot:///user_profile/brianvs"]];
+    }  else {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://twitter.com/brianvs"]];
+    }
+}
+/*
+-(void)respring {
+         system("killall -9 SpringBoard");
+      }
+*/
+- (void)Paypal
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://paypal.me/i0stweak3r"]];
+}
+
+@end
+
+// vim:ft=objc
